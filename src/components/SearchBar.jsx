48166fa3d168052ce531edaef1416ab370ce0a97
jerico-esguerra <jericoesguerra212@outlook>
2023-01-16 13:37:55 -0500

    minor style changes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -23,7 +23,7 @@ const SearchBar = () => {
             onSubmit={onhandleSubmit}
             sx={{
                 borderRadius: 20,
-                border: '1px solid #e3e3e3',
+                border: '0.0625rem solid #e3e3e3',
                 pl: 2,
                 boxShadow: 'none',
                 mr: { sm: 5 },
@@ -35,7 +35,7 @@ const SearchBar = () => {
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
             />
-            <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+            <IconButton type='submit' sx={{ p: '0.625rem', color: 'red' }} aria-label='search'>
                 <SearchIcon />
             </IconButton>
         </Paper>
